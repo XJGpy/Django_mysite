401100d4fdaa0cce40eef8e78b2739070d58ad0e
xue <xue@xue.(none)>
2017-06-28 15:47:59 +0800

    add some views

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+	#return HttpResponse("Welcome to my Blog!")
+	post_list = Post.objects.all().order_by('-created_time')
+	return render(request, 'blog/index.html', context={
+			#'title': 'My Blog', 
+			#'welcome': 'Welcome to my blog!!'
+			'post_list': post_list
+	})
+
